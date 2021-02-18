1ca5bcbdf4c82fedddc8fb7baa629c2f928dfaac
Laurenz Gerold <skatezar@gmail.com>
2021-02-18 14:55:36 +0100

    rake works, working on controller view and routing next for cocktails CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Ingredient.create(name: "tomato")
+Ingredient.create(name: "apple")
+
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+
